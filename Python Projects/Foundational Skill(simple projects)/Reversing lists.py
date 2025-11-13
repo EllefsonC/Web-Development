b32aa02c9b91b11e3475568e3deb9b26a9ac42d7
Christian Ellefson <christianellefson820@gmail.com>
2025-11-13 18:30:43 -0500

    Enhance list reversing example with additional output
    Added example of reversing a list using the built-in reverse method.

--- Python Projects/Foundational Skill(simple projects)/Reversing lists.py
+++ Python Projects/Foundational Skill(simple projects)/Reversing lists.py
@@ -14,5 +14,8 @@ print(r)
 #The reversed_list() function works as a method of slicing, where the start and stop args are empty, meaning for the whole list to be iterated through, and the step is -1 which means it will iterate through in reverse and return the reversed list either to the original variable as was done in this example, or to a new variable. 
 
 #this is the built in and simplest way to reverse a list but there are always multiple approachs, and the built in methods may not always be the least resource intensive or efficient in O time.
-r.reverse()
-print(r)
+print(' ')
+l=list(range(0, 11, 2))
+print(l)
+l.reverse()
+print(l)
